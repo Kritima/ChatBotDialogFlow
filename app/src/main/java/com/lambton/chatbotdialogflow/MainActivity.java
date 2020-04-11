5906b6472a6f87b58618e0615b2fc38863add650
Kritima Kukreja <kritimakukreja5@gmail.co>
2020-04-11 15:04:40 -0400

    Request audio permission function added

--- app/src/main/java/com/lambton/chatbotdialogflow/MainActivity.java
+++ app/src/main/java/com/lambton/chatbotdialogflow/MainActivity.java
@@ -1,11 +1,17 @@
 package com.lambton.chatbotdialogflow;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.gson.JsonElement;
 
@@ -19,6 +25,8 @@ import ai.api.model.AIResponse;
 import ai.api.model.Result;
 
 public class MainActivity extends AppCompatActivity implements AIListener {
+
+    private final int MY_PERMISSIONS_RECORD_AUDIO = 1;
 
     private AIService aiService;
     Button listenButton;
@@ -38,12 +46,9 @@ public class MainActivity extends AppCompatActivity implements AIListener {
 
         aiService = AIService.getService(this, config);
         aiService.setListener(this);
+        requestAudioPermissions();
 
 
-    }
-
-    public void listenButtonOnClick(final View view) {
-        aiService.startListening();
     }
 
     @Override
@@ -79,6 +84,7 @@ public class MainActivity extends AppCompatActivity implements AIListener {
 
     @Override
     public void onListeningStarted() {
+            System.out.println("hi");
 
     }
 
@@ -89,6 +95,62 @@ public class MainActivity extends AppCompatActivity implements AIListener {
 
     @Override
     public void onListeningFinished() {
+        System.out.println("bYE");
 
+    }
+    private void requestAudioPermissions() {
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.RECORD_AUDIO)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            //When permission is not granted by user, show them message why this permission is needed.
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.RECORD_AUDIO)) {
+                Toast.makeText(this, "Please grant permissions to record audio", Toast.LENGTH_LONG).show();
+
+                //Give user option to still opt-in the permissions
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.RECORD_AUDIO},
+                        MY_PERMISSIONS_RECORD_AUDIO);
+
+            } else {
+                // Show user dialog to grant permission to record audio
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.RECORD_AUDIO},
+                        MY_PERMISSIONS_RECORD_AUDIO);
+            }
+        }
+        //If permission is granted, then go ahead recording audio
+        else if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.RECORD_AUDIO)
+                == PackageManager.PERMISSION_GRANTED) {
+
+            //Go ahead with recording audio now
+        }
+    }
+
+    public void listenButtonOnClick(final View view) {
+        aiService.startListening();
+    }
+
+
+    //Handling callback
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_RECORD_AUDIO: {
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // permission was granted, yay!
+
+                } else {
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                    Toast.makeText(this, "Permissions Denied to record audio", Toast.LENGTH_LONG).show();
+                }
+                return;
+            }
+        }
     }
 }
