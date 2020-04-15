6af95125f757d348bcbca1a2dc4270c7c9e207d3
Kritima Kukreja <kritimakukreja5@gmail.co>
2020-04-14 22:17:38 -0400

    Text Functionality added

--- app/src/main/java/com/lambton/chatbotdialogflow/MainActivity.java
+++ app/src/main/java/com/lambton/chatbotdialogflow/MainActivity.java
@@ -7,147 +7,95 @@ import androidx.core.content.ContextCompat;
 import android.Manifest;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.text.Editable;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.gson.JsonElement;
 
 import java.util.Map;
+import java.util.UUID;
 
 import ai.api.AIListener;
+import ai.api.AIServiceContext;
+import ai.api.AIServiceContextBuilder;
+import ai.api.BuildConfig;
 import ai.api.android.AIConfiguration;
+import ai.api.android.AIDataService;
 import ai.api.android.AIService;
 import ai.api.model.AIError;
+import ai.api.model.AIRequest;
 import ai.api.model.AIResponse;
 import ai.api.model.Result;
 
-public class MainActivity extends AppCompatActivity implements AIListener {
+public class MainActivity extends AppCompatActivity  {
 
-    private final int MY_PERMISSIONS_RECORD_AUDIO = 1;
+    private String uuid = UUID.randomUUID().toString();
+    private static final String TAG = MainActivity.class.getSimpleName();
 
-    private AIService aiService;
-    Button listenButton;
+    Button submitButton;
     private TextView resultTextView;
+    EditText queryEditText;
+    private AIRequest aiRequest;
+    private AIDataService aiDataService;
+    private AIServiceContext customAIServiceContext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        listenButton = findViewById(R.id.button);
+        submitButton = findViewById(R.id.button);
         resultTextView = findViewById(R.id.textView);
+        queryEditText = findViewById(R.id.editText);
 
+       initChatbot();
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendMessage(v);
+            }
+        });
+    }
+
+    private void initChatbot() {
         final AIConfiguration config = new AIConfiguration("e194dd6256564e10a03f2e07db49619d",
                 AIConfiguration.SupportedLanguages.English,
                 AIConfiguration.RecognitionEngine.System);
 
-        aiService = AIService.getService(this, config);
-        aiService.setListener(this);
-        requestAudioPermissions();
+        aiDataService = new AIDataService(this, config);
+        customAIServiceContext = AIServiceContextBuilder.buildFromSessionId(uuid);// helps to create new session whenever app restarts
+        aiRequest = new AIRequest();
     }
 
-    @Override
-    public void onResult(final AIResponse response) {
+    private void sendMessage(View view) {
+        String msg = queryEditText.getText().toString();
+        if (msg.trim().isEmpty()) {
+            Toast.makeText(MainActivity.this, "Please enter your query!", Toast.LENGTH_LONG).show();
+        } else {
+            queryEditText.setText("");
+            aiRequest.setQuery(msg);
+           RequestTask requestTask = new RequestTask(MainActivity.this, aiDataService, customAIServiceContext);
+           requestTask.execute(aiRequest);
 
-        Result result = response.getResult();
-
-        // Get parameters
-        String parameterString = "";
-        if (result.getParameters() != null && !result.getParameters().isEmpty()) {
-            for (final Map.Entry<String, JsonElement> entry : result.getParameters().entrySet()) {
-                parameterString += "(" + entry.getKey() + ", " + entry.getValue() + ") ";
-            }
-        }
-
-        // Show results in TextView.
-        resultTextView.setText("Query:" + result.getResolvedQuery() +
-                "\nResponse: " + response.getResult().getFulfillment().getSpeech()+
-                "\nAction: " + result.getAction() +
-                "\nParameters: " + parameterString);
-    }
-
-    @Override
-    public void onError(final AIError error) {
-        resultTextView.setText(error.toString());
-
-    }
-
-    @Override
-    public void onAudioLevel(float level) {
-
-    }
-
-    @Override
-    public void onListeningStarted() {
-            System.out.println("hi");
-
-    }
-
-    @Override
-    public void onListeningCanceled() {
-
-    }
-
-    @Override
-    public void onListeningFinished() {
-        System.out.println("bYE");
-
-    }
-    private void requestAudioPermissions() {
-        if (ContextCompat.checkSelfPermission(this,
-                Manifest.permission.RECORD_AUDIO)
-                != PackageManager.PERMISSION_GRANTED) {
-
-            //When permission is not granted by user, show them message why this permission is needed.
-            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
-                    Manifest.permission.RECORD_AUDIO)) {
-                Toast.makeText(this, "Please grant permissions to record audio", Toast.LENGTH_LONG).show();
-
-                //Give user option to still opt-in the permissions
-                ActivityCompat.requestPermissions(this,
-                        new String[]{Manifest.permission.RECORD_AUDIO},
-                        MY_PERMISSIONS_RECORD_AUDIO);
-
-            } else {
-                // Show user dialog to grant permission to record audio
-                ActivityCompat.requestPermissions(this,
-                        new String[]{Manifest.permission.RECORD_AUDIO},
-                        MY_PERMISSIONS_RECORD_AUDIO);
-            }
-        }
-        //If permission is granted, then go ahead recording audio
-        else if (ContextCompat.checkSelfPermission(this,
-                Manifest.permission.RECORD_AUDIO)
-                == PackageManager.PERMISSION_GRANTED) {
-
-            //Go ahead with recording audio now
         }
     }
 
-    public void listenButtonOnClick(final View view) {
-        aiService.startListening();
-    }
-
-    //Handling callback
-    @Override
-    public void onRequestPermissionsResult(int requestCode,
-                                           String permissions[], int[] grantResults) {
-        switch (requestCode) {
-            case MY_PERMISSIONS_RECORD_AUDIO: {
-                if (grantResults.length > 0
-                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    // permission was granted, yay!
-
-                } else {
-                    // permission denied, boo! Disable the
-                    // functionality that depends on this permission.
-                    Toast.makeText(this, "Permissions Denied to record audio", Toast.LENGTH_LONG).show();
-                }
-                return;
-            }
+    public void callback(AIResponse aiResponse) {
+        if (aiResponse != null) {
+            // process aiResponse here
+            String botReply = aiResponse.getResult().getFulfillment().getSpeech();
+            Log.d(TAG, "Bot Reply: " + botReply);
+           resultTextView.setText(botReply);
+        } else {
+            Log.d(TAG, "Bot Reply: Null");
+            resultTextView.setText("There was some communication issue. Please Try again!");
         }
     }
+
+
 }
